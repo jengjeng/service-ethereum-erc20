50e609cf5c11493f146774715695c147ae2179a1
Nicolas Mahe <nicolas@mahe.me>
2018-06-25 16:45:39 +0700

    Move config to a dedicated file

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const ERC20_ADDRESS = "0xf230b790e05390fc8295f4d3f60332c93bed42e2" //TRON
-const ERC20_DECIMAL = 6
-const ERC20_ABI = require('./erc20-abi.json')
+const MESG = require('mesg-js').service()
+
+const { erc20Address, erc20Decimal, infuraEndpoint } = require('./config.json')
+const erc20ABI = require('./erc20-abi.json')
 
 const Web3 = require('web3')
-const web3 = new Web3('wss://mainnet.infura.io/_ws')
-const erc20 = new web3.eth.Contract(ERC20_ABI, ERC20_ADDRESS)
+const web3 = new Web3(infuraEndpoint)
+const erc20 = new web3.eth.Contract(erc20ABI, erc20Address)
 
 erc20.events.Transfer({fromBlock: 'latest'})
 .on('data', event => {
