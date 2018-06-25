72824923ebc8491c887705146b7ea14edf5f4e3f
Nicolas Mahe <nicolas@mahe.me>
2018-06-25 16:46:58 +0700

    Add event transfer, approval and functions totalSupply, balanceOf, allowance

--- index.js
+++ index.js
@@ -7,13 +7,71 @@ const Web3 = require('web3')
 const web3 = new Web3(infuraEndpoint)
 const erc20 = new web3.eth.Contract(erc20ABI, erc20Address)
 
+convertValue = value => value / Math.pow(10, erc20Decimal)
+
+reportError = console.error
+
 erc20.events.Transfer({fromBlock: 'latest'})
 .on('data', event => {
-  console.log('Transfer', {
+  return MESG.emitEvent('transfer', {
     blockNumber: event.blockNumber,
     transactionHash: event.transactionHash,
     from: event.returnValues.from,
     to: event.returnValues.to,
-    value: event.returnValues.value / Math.pow(10, ERC20_DECIMAL),
+    value: convertValue(event.returnValues.value),
   })
 })
+.on('error', reportError)
+
+erc20.events.Approval({fromBlock: 'latest'})
+.on('data', event => {
+  return MESG.emitEvent('approval', {
+    blockNumber: event.blockNumber,
+    transactionHash: event.transactionHash,
+    owner: event.returnValues.owner,
+    spender: event.returnValues.spender,
+    value: convertValue(event.returnValues.value),
+  })
+})
+.on('error', reportError)
+
+const totalSupply = ({}, { success, error }) => {
+  return erc20.methods.totalSupply().call()
+  .then(totalSupply => {
+    totalSupply = convertValue(totalSupply)
+    console.log("totalSupply", totalSupply)
+    return success({totalSupply})
+  })
+  .catch(err => {
+    reportError(err)
+    return error({message: err.toString()})
+  })
+}
+
+const balanceOf = ({ address }, { success, error }) => {
+  return erc20.methods.balanceOf(address).call()
+  .then(balance => {
+    balance = convertValue(balance)
+    console.log("balance of", address, "is", balance)
+    return success({balance})
+  })
+  .catch(err => {
+    reportError(err)
+    return error({message: err.toString()})
+  })
+}
+
+const allowance = ({ owner, spender }, { success, error }) => {
+  return erc20.methods.allowance(owner, spender).call()
+  .then(remaining => {
+    remaining = convertValue(remaining)
+    console.log("remaining allowance between", owner, "and", spender, "is", remaining)
+    return success({remaining})
+  })
+  .catch(err => {
+    reportError(err)
+    return error({message: err.toString()})
+  })
+}
+
+MESG.listenTask({ totalSupply, balanceOf, allowance })
